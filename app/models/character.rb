88970a5826f362fc6a9ee0ffa51db84bb82d5d86
github username <github email address>
2020-07-25 17:54:18 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+    def say_that_thing_you_say
+       pharse = self.catchphrase
+       name = self.name
+       "#{name} always says: #{pharse}"
+    end
+
+
 end
