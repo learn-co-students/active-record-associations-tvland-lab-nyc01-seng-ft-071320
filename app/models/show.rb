9a6568dc5ecc771d270c6008ff0e642b43dc4b82
amitdesh <amitdesh2014@gmail.com>
2020-07-26 16:15:51 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    Character.all.map{|char| char.actor.full_name if char.show_id == self.id}
+  end
+
 end
